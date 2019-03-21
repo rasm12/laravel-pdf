17febea860c2e301c70926aeb6fbd18c3c359d01
LAPTOP-OJDUVEM9\ACER <rsanchez@monchis.com.py>
2019-03-21 20:27:01 -0300

    agrega log

--- app/Http/Controllers/ReadPdfController.php
+++ app/Http/Controllers/ReadPdfController.php
@@ -55,6 +55,8 @@ class ReadPdfController extends Controller {
 
         $parser = new \Smalot\PdfParser\Parser();
         $sourcefile = public_path() . '\\uploads\\' . $file->getClientOriginalName();
+        
+        error_log("Path file $sourcefile");
 
         $pdf = $parser->parseFile($sourcefile);
         try {
